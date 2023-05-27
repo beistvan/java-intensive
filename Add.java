f0a19e0c163d502900ef2c18fd712cf3f91d7422
Beregszászi István <beistvan@gmail.com>
2023-05-27 17:37:44 +0200

    Add Java

---
+++ Add.java
@@ -0,0 +1,16 @@
+class Add {
+    public static void main(String[] args) {
+        int a = 13;
+        int b;
+
+        b = 103;
+
+        int result = a + b;
+
+        System.out.println("a + b =");
+        System.out.println(result);
+
+        int year = 2023;
+        System.out.println(year);
+    }
+}
