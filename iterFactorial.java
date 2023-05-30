2e7dd6305c1d83e5798d564e278fc97adbba3c01
Beregszászi István <beistvan@gmail.com>
2023-05-30 02:42:52 +0200

    Iterative Factorial

--- iterFactorial.java
+++ iterFactorial.java
@@ -1,7 +1,7 @@
 public static int iterFactorial(int n) {
-    if(n == 1){
-        return 1;
-    }else{
-        return n * iterFactorial(n - 1);
+    int factorial = 1;
+    for (int i = 1; i <= n; i++) {
+        factorial *= i;
     }
+    return factorial;
 }
